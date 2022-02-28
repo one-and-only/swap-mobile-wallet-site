764be3f3485e3779953f7582236fe61450fbe9c2
Antonios Papadakis <bdpaboi123@gmail.com>
2022-02-27 18:04:47 -0600

    Finish Website
    `Download On The App Store` leads to TestFlight page, will update once app gets launched in the App Store

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,8 @@
 import Header from "./header";
+import Image from "next/image";
+import phoneImage from "../public/images/phone.webp";
+import appStoreDownload from "../public/images/download_on_the_app_store.webp";
+import Link from "next/link";
 
 /*const swapGradient: {from: string, to: string, deg: number} = {
   from: "#a760f8",
@@ -10,6 +14,35 @@ export default function Home({ redirectReason }): JSX.Element {
   return (
     <div>
       <Header title="Swap Mobile Wallet" />
+      <div id="app-demo">
+        <Image
+          src={phoneImage}
+          width={350}
+          height={711}
+          alt="Phone Demo Image"
+          id="demo-image"
+        />
+        <div id="demo-text">
+          <h2 id="promo-text">
+            The Most Advanced Swap Mobile Wallet.{" "}
+            <span style={{ color: "#0ab1ff" }}>Ever.</span>
+          </h2>
+          <h3 id="promo-description">
+            Quickly and privately manage and transfer $XWP from the convenience
+            of your phone
+          </h3>
+          <Link href="https://testflight.apple.com/join/veCyui9K">
+            <a target="_blank">
+              <Image
+                src={appStoreDownload}
+                alt="Download on the App Store"
+                width={300}
+                height={100}
+              />
+            </a>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
